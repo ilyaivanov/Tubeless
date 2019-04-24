c11cabce0c9b72e13d08966dcc5a2efff547434c
Ilya <illiai@wix.com>
2019-04-24 17:30:02 +0300

    Improve rules for restrictive drag and drop

--- src/tree/rules.spec.ts
+++ src/tree/rules.spec.ts
@@ -1,4 +1,4 @@
-import { canDragOver, convertPlacement } from "./rules";
+import {canDragOver} from "./rules";
 import {Placement} from "./types";
 import {createTwoFlatNodes} from "./nodes";
 
@@ -13,20 +13,3 @@ describe("having a two nodes", () => {
     expect(canDragOver(createTwoFlatNodes(), placement)).toBe(true);
   });
 });
-//
-// describe("having a two nodes", () => {
-//   const nodes = [
-//     node("Item 1", [node("Item 1.1")]),
-//     node("Item 2")
-//   ];
-//
-//   it("should be possible to place Item 2 before Item 1", () => {
-//     const placement: Placement = {
-//       relativeShift: 1,
-//       highlightPlacement: "PLACE_BEFORE",
-//       id: "Item 1.1",
-//       itemBeingDragged: "Item 2"
-//     };
-//     expect(convertPlacement(nodes, placement).relativeShift).toEqual(0);
-//   });
-// });
