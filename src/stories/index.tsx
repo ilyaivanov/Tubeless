fdcdedc72195ffc546f753e9bcaf4949593717da
Ilya <illiai@wix.com>
2019-04-24 12:21:31 +0300

    Add simple static markup

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import {storiesOf} from '@storybook/react';
 
-import {MyButton} from "../App";
+storiesOf('Tree', module)
+  .add('tree', () => <SampleTree/>)
 
 
-storiesOf('Button', module)
-  .add('even', () => <MyButton onPress={action('clicked even')} isEven={true}  />)
-  .add('odd', () => <MyButton onPress={action('clicked odd')} isEven={false}  />);
+const Node = ({level, text}: { level?: number, text: string }) => (
+  <div style={{marginLeft: level ? level * 30 : 0, height: 30, alignItems: 'center', display: 'flex'}}>
+    <div style={{height: 10, width: 10, backgroundColor: '#000000', borderRadius: 10}}/>
+    <span style={{marginLeft: 10}}>{text}</span>
+  </div>
+);
+
+
+const SampleTree = () => (
+  <div>
+    <Node text="Item 1"/>
+    <Node text="Something" level={1}/>
+    <Node text="beautiful" level={2}/>
+    <Node text="and available" level={1}/>
+    <Node text="only via" level={2}/>
+    <Node text="keyboard" level={2}/>
+    <Node text="goo"/>
+    <Node text="Item 2"/>
+  </div>
+)
