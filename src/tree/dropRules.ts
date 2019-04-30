65eac24597ab41092aa5a06b92feb08f34621443
Ilya Ivanov <static.ila@gmail.com>
2019-04-30 22:34:55 +0300

    Handle generic case when placing items in flat list before target item

--- src/tree/dropRules.ts
+++ src/tree/dropRules.ts
@@ -2,12 +2,21 @@ import { Nodes, Placement, Tree } from "./types";
 import { except } from "./rules";
 
 
-export function handleDrop(tree: Tree, action: Placement): Tree{
+export function handleDrop(tree: Tree, action: Placement): Tree {
     const nodes = {
         ...tree.nodes
     }
+    const index = tree.roots.indexOf(action.id);
+    //place itemBeingDragged into index position
     return {
         nodes,
-        roots: [action.itemBeingDragged].concat(except(tree.roots, action.itemBeingDragged)),
+        roots: moveItemIntoIndex(tree.roots, action.itemBeingDragged, index)
     };
-  }
+}
+
+//TODO: handle case when moving item forward or backward
+function moveItemIntoIndex<T>(items: T[], itemToMove: T, targetIndex: number): T[] {
+    const removed = except([...items], itemToMove);
+    removed.splice(targetIndex, 0, itemToMove);
+    return removed;
+}
