8bd496c68d4fa61dcb0c83d63384f50eabd8562a
Ilya Ivanov <static.ila@gmail.com>
2019-04-30 22:34:55 +0300

    Handle generic case when placing items in flat list before target item

--- src/tree/dropRules.spec.ts
+++ src/tree/dropRules.spec.ts
@@ -1,26 +1,25 @@
 import { Placement, Tree } from "./types";
 import { handleDrop } from "./dropRules";
 
-const threeNodes = {
-    '1': {
-        id: '1',
-        text: 'First'
+const threeNodes: Tree = {
+    nodes: {
+        '1': {
+            id: '1',
+            text: 'First'
+        },
+        '2': {
+            id: '2',
+            text: 'Second'
+        },
+        '3': {
+            id: '3',
+            text: 'Third'
+        },
     },
-    '2': {
-        id: '2',
-        text: 'Second'
-    },
-    '3': {
-        id: '3',
-        text: 'Third'
-    },
-}
+    roots: ['1', '2', '3']
+};
 
 it('when droping second item before first it should place second item at the beggining', () => {
-    const tree: Tree = {
-        nodes: threeNodes,
-        roots: ['1', '2']
-    };
     const action: Placement = {
         itemBeingDragged: '2',
         highlightPlacement: 'PLACE_BEFORE',
@@ -28,8 +27,22 @@ it('when droping second item before first it should place second item at the beg
         relativeShift: 0
     };
     const expectedTree = {
-        nodes: tree.nodes,
-        roots: ['2', '1']
+        nodes: threeNodes.nodes,
+        roots: ['2', '1', '3']
     };
-    expect(handleDrop(tree, action)).toEqual(expectedTree);
+    expect(handleDrop(threeNodes, action)).toEqual(expectedTree);
 })
+
+
+it('when droping third item before second it should place third item between second and first', () => {
+    const action: Placement = {
+        itemBeingDragged: '3',
+        highlightPlacement: 'PLACE_BEFORE',
+        id: '2',
+        relativeShift: 0
+    };
+    const expectedRoots = ['1', '3', '2'];
+
+    expect(handleDrop(threeNodes, action).roots).toEqual(expectedRoots);
+})
+
