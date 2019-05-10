a2e0d742a1a91cf7ff0eddf10c600e578f9589e7
Ilya <illiai@wix.com>
2019-05-10 19:41:39 +0300

    Remove all

--- src/dndTree/DragDropContext.js
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from "react";
-import { DragDropContext } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
-
-const Context = ({ children }) => <Fragment>{children}</Fragment>;
-
-export default DragDropContext(HTML5Backend)(Context);
