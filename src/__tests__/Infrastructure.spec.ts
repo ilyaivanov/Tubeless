02eb7251dda750d4491c56a6206c4243e0bd253d
Ilya <illiai@wix.com>
2019-05-16 14:17:49 +0300

    Add better error messages

---
+++ src/__tests__/Infrastructure.spec.ts
@@ -0,0 +1,19 @@
+import AppPageObject from "../testUtils/AppPageObject";
+import { Roots } from "../tree/types";
+
+describe("When rendering a non-existing tree (usually set as children) a meaningful error should be thrown ", () => {
+  fit("sample", () => {
+    expect(() => {
+      const page = new AppPageObject({
+        favoriteFirstNodes: ["non-existing1"]
+      });
+      page.render();
+    }).toThrowError(
+      `Node '${
+        Roots.FAVORITES
+      }' contains a non-existing child 'non-existing1'. Check children of '${
+        Roots.FAVORITES
+      }'`
+    );
+  });
+});
