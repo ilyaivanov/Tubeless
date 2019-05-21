209ef3fb4c7e8555d2285a73f23a4f51657d6626
Ilya <illiai@wix.com>
2019-05-21 14:50:08 +0300

    Fix visuals. Extract VideoPreview image

---
+++ src/tree/components/VideoPreview.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const VideoPreview = styled.img`
+  height: 32px;
+  width: 32px;
+  margin-right: 5px;
+`;
--- src/tree/components/index.ts
+++ src/tree/components/index.ts
@@ -4,4 +4,5 @@ export { Bullet } from "./Bullet";
 export { NodeContainer } from "./NodeContainer";
 export { NodeText } from "./NodeText";
 export { NodeIcons } from "./NodeIcons";
+export { VideoPreview } from "./VideoPreview";
 export { ARROW_SIZE, LEVEL_SHIFT } from "./constants";
