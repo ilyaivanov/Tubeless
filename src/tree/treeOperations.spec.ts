58a37d86612122db97d7e93edb2142fa4833d01e
Ilya <illiai@wix.com>
2019-07-17 14:48:36 +0300

    Add unit tests for getting next item

--- src/tree/treeOperations.spec.ts
+++ src/tree/treeOperations.spec.ts
@@ -1,6 +1,6 @@
 import { Nodes } from "./types";
 import { node } from "./sampleTrees";
-import { copyNodeDeep } from "./treeOperations";
+import { copyNodeDeep, getNextNodeId } from "./treeOperations";
 
 it("it should also copy all subnodes", () => {
   const nodes: Nodes = {
@@ -17,4 +17,21 @@ it("it should also copy all subnodes", () => {
   expect(res["newParent"].text).toEqual("Node parent");
   expect(res["newChild"].text).toEqual("Node child");
   expect(res["newGrandChild"].text).toEqual("Node grand-child");
+});
+
+describe("having a flat list of three nodes", () => {
+  const nodes: Nodes = {
+    ...node("root", ["node1", "node2", "node3"]),
+    ...node("node1"),
+    ...node("node2"),
+    ...node("node3")
+  };
+
+  it("getting next item for the second one should return third", () => {
+    expect(getNextNodeId(nodes, "node2")).toEqual("node3");
+  });
+
+  it('getting next item for the third one should return nothing', () => {
+    expect(getNextNodeId(nodes, "node3")).toBeUndefined();
+  });
 });
